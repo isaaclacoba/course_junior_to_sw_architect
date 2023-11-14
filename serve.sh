8f31caa6ae013eabb9464b7abf31331b68418cdb
A <a@b>
2023-11-14 22:13:20 +0000

    Serve with no-store, so a reload always shows what is on disk

--- serve.sh
+++ serve.sh
@@ -2,10 +2,18 @@
 # One-click launcher for the training site.
 # A browser cannot start a server by itself (security sandbox), so this small
 # script starts a local web server and then opens the site for you.
+#
+# It sends "Cache-Control: no-store" on everything. Plain `python3 -m http.server`
+# sends no cache header at all, so browsers fall back to heuristic caching and
+# happily serve a lesson's data file from memory minutes after it changed. That
+# has misled three content reviews: an edit was on disk, in the commit and on the
+# wire, and the page still showed the old text through repeated reloads.
 cd "$(dirname "$0")" || exit 1
-URL="http://localhost:8080"
+PORT="${1:-8080}"
+URL="http://localhost:$PORT"
 echo
-echo "  Starting the training site..."
+echo "  Starting the training site on $URL"
+echo "  Nothing is cached, so a reload always shows what is on disk."
 echo "  Keep this window open while you use it. Press Ctrl+C to stop."
 echo
 
@@ -15,4 +23,15 @@ echo
   elif command -v xdg-open >/dev/null 2>&1; then xdg-open "$URL" # Linux
   fi ) &
 
-python3 -m http.server 8080
+python3 - "$PORT" <<'PY'
+import sys
+from http.server import SimpleHTTPRequestHandler, ThreadingHTTPServer
+
+class NoCache(SimpleHTTPRequestHandler):
+    def end_headers(self):
+        self.send_header("Cache-Control", "no-store, must-revalidate")
+        self.send_header("Expires", "0")
+        super().end_headers()
+
+ThreadingHTTPServer(("", int(sys.argv[1])), NoCache).serve_forever()
+PY
